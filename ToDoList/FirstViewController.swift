efa755cb3dea73bcd8cb6e4470cba2f28f2e1009
Andreas Schmitt <aschmitt@Andreass-MacBook-Pro.local>
2017-04-11 22:38:05 +0200

    first commit

--- ToDoList/FirstViewController.swift
+++ ToDoList/FirstViewController.swift
@@ -8,8 +8,36 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+var list = ["Test1" , "test 2"]
 
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = list[indexPath.row]
+        return cell
+    }
+    
+    public func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            list.remove(at: indexPath.row)
+            myTableView.reloadData()
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        myTableView.reloadData()
+        tabBarController?.tabBar.items?[0].badgeValue = "\(list.count)"
+    }
+
+    @IBOutlet weak var myTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
--- ToDoList/SecondViewController.swift
+++ ToDoList/SecondViewController.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var input: UITextField!
+    
+    @IBAction func addItem(_ sender: Any) {
+        if let toDoText = input.text {
+            if input.text != "" {
+                list.append(toDoText)
+                input.text = ""
+                input.placeholder = "New ToDo please"
+                tabBarController?.tabBar.items?[0].badgeValue = "\(list.count)"
+            } else {
+                let alert = UIAlertController(title: "Hey", message: "This is  one Alert", preferredStyle: UIAlertControllerStyle.alert)
+                alert.addAction(UIAlertAction(title: "Working!!", style: UIAlertActionStyle.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.input.delegate = self;
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(SecondViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
 
+    func dismissKeyboard() {
+         view.endEditing(true)
+    }
 }
 
